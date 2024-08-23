75dbc3ad5d60008740a0e37ccc43d61fbecc42f3
flexerblex <lilysuau@gmail.com>
2024-08-23 09:35:44 -0400

    compile

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -6,49 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
-    data: any;
-    introText: string;
+  public data: any;
+  public introText: string;
 
-  constructor() { 
+  constructor() {
     this.introText = 'For over 3 years, I\'ve developed websites that are responsive, easy to use, and built with best practices. '
-    + 'I try to create suited, simple and tested experiences to adapt the desires of companies to the needs of users.';
-    // this.introText = 'For over 3 years, I\'ve nurtured a strong Design expertise that puts the user first. '
-    // + 'On all my projects, I try to create suited, simple and tested experiences to adapt the desires of companies to the needs of users.';
+      + 'I try to create suited, simple and tested experiences to adapt the desires of companies to the needs of users.';
     this.data = [
-        {
-            position: 'Developer I at ',
-            company: 'TelNet Worldwide',
-            link: 'https://www.telnetww.com',
-            description: 'I currently lead the development of an internal customer portal and reporting tool. I also work on optimizing performance by restructuring stored procedures and preloading components/APIs.',
-            more: '',
-            stack: ['Angular', 'TypeScript', 'ASP.NET', 'ElasticSearch', 'Jenkins', 'Wordpress'],
-            year: '2021'
-        },
-        {
-            position: 'Graduated at ',
-            company: 'Oakland University',
-            link: 'https://en.wikipedia.org/wiki/Oakland_University',
-            description: 'I completed my Computer Science degree with a concentration in cyber security!',
-            more: '',
-            stack: [],
-            year: '2020'
-        },
-        {
-            position: 'Software Developer Co-op at ',
-            company: 'Bosch',
-            link: 'https://www.bosch.us',
-            description: 'I assisted on the development of an internal application and designed personalized dashboards using Power BI.',
-            more: '',
-            stack: ['ASP.NET', 'Hangfire', 'JQuery'],
-            year: '2018'
-        }
+      {
+        position: 'Developer II at ',
+        company: 'Brandt Information Services',
+        link: 'https://www.brandtinfo.com',
+        description: 'I currently assist in the development of an outdoor recreational management application. I develop key features such as controlled hunt draws, youth programs, and organization management.',
+        more: '',
+        stack: ['Angular', 'TypeScript', 'ASP.NET', 'Azure'],
+        year: '2023'
+      },
+      {
+        position: 'Developer I at ',
+        company: 'TelNet Worldwide',
+        link: 'https://www.telnetww.com',
+        description: 'I lead the development of an internal customer portal and reporting tool. I also worked on optimizing performance by restructuring stored procedures and preloading components/APIs.',
+        more: '',
+        stack: ['Angular', 'TypeScript', 'ASP.NET', 'ElasticSearch', 'Jenkins', 'Wordpress'],
+        year: '2021'
+      },
+      {
+        position: 'Graduated at ',
+        company: 'Oakland University',
+        link: 'https://en.wikipedia.org/wiki/Oakland_University',
+        description: 'I completed my Computer Science degree with a concentration in cyber security!',
+        more: '',
+        stack: [],
+        year: '2020'
+      },
+      {
+        position: 'Software Developer Co-op at ',
+        company: 'Bosch',
+        link: 'https://www.bosch.us',
+        description: 'I assisted on the development of an internal application and designed personalized dashboards using Power BI.',
+        more: '',
+        stack: ['ASP.NET', 'Hangfire', 'JQuery'],
+        year: '2018'
+      }
     ]
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
   }
 
-  downloadResume() {
+  public downloadResume(): void {
     const link = document.createElement('a');
     link.setAttribute('target', '_blank');
     link.setAttribute('href', 'assets/images/lilia-suau_resume.pdf');
@@ -57,5 +64,4 @@ export class ExperienceComponent implements OnInit {
     link.click();
     link.remove();
   }
-
 }
