92b117da567a2607a016eec1f171a86b9426a1d1
flexerblex <lilysuau@gmail.com>
2024-08-23 09:49:42 -0400

    remove confetti :(

--- src/app/services/confetti.service.ts
+++ src/app/services/confetti.service.ts
@@ -6,22 +6,22 @@ import { Injectable } from '@angular/core';
 })
 export class ConfettiService {
     public celebrate() {
-        var myCanvas = document.createElement('canvas');
-        document.body.prepend(myCanvas);
+        // var myCanvas = document.createElement('canvas');
+        // document.body.prepend(myCanvas);
 
-        var myConfetti = myConfetti.create(myCanvas, {
-            resize: true,
-            useWorker: true
-        });
+        // var myConfetti = myConfetti.create(myCanvas, {
+        //     resize: true,
+        //     useWorker: true
+        // });
 
-        myConfetti({
-            particleCount: 200,
-            spread: 1000,
-            origin: { y: 0.6 },
-            colors: ['#FF4500', '#008080', '#FFD700'],
-        });
+        // myConfetti({
+        //     particleCount: 200,
+        //     spread: 1000,
+        //     origin: { y: 0.6 },
+        //     colors: ['#FF4500', '#008080', '#FFD700'],
+        // });
     }
-    
+
     // public celebrate() {
     //     const duration = 10000;
 
